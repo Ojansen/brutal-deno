a5f0c491debfc9f7c7fbf24c7841fe15d9b42873
Ojansen <obejansen7@gmail.com>
2024-03-01 19:10:51 +0100

    feat: Install tailwind

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
-import twindPlugin from "$fresh/plugins/twind.ts"
-import twindConfig from "./twind.config.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
+
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)]
+  server: {
+    port: 3000,
+  },
+  plugins: [tailwind()],
 });
